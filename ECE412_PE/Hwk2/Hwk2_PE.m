3ffb18c516ca80d5047606d3581c41030a92feff
Phil_Baik_Mac <hmbaik97@gmail.com>
2023-10-10 14:46:16 -0500

    Feat: ECE412 HW #2 Code
    ECE412 Power Electronics HW#2 Done

--- ECE412_PE/Hwk2/Hwk2_PE.m
+++ ECE412_PE/Hwk2/Hwk2_PE.m
@@ -32,3 +32,45 @@ Id = Io-D*Io
 Id_RMS = sqrt(1-D)*Io
 id_peak = Io
 Pd_cond = VF*Io*(1-D)
+
+%% Q3
+fs_low = 1e3;
+fs_high = 500e3;
+
+Ps_swit1 = fs_low*(Woff+Won)
+Ps_swit2 = fs_high*(Woff+Won)
+Ps_tot1 = Ps_swit1 + Ps_cond
+Ps_tot2 = Ps_swit2 + Ps_cond
+
+RthJA = 40;
+RthJC = 0.83;
+RthCS = 0.24;
+Rthcase = 4;
+RthJA_heat_sink = RthJC+RthCS+Rthcase;
+
+
+Tdiff1 = RthJA*Ps_tot1
+Tdiff2 = RthJA*Ps_tot2
+
+Tdiff1_case = RthJA_heat_sink*Ps_tot1
+Tdiff2_case = RthJA_heat_sink*Ps_tot2
+
+P_tot1 = Ps_tot1 + Pd_cond
+P_tot2 = Ps_tot2 + Pd_cond
+
+Pin = Vin*Is
+
+Po1 = Pin-P_tot1
+Po2 = Pin-P_tot2
+
+n1 = Po1/Pin
+n2 = Po2/Pin
+
+Vo1 = Po1/Io
+Vo2 = Po2/Io
+
+%% q5
+temp_diff_max = 125
+Ps_max = temp_diff_max/RthJA_heat_sink
+Ps_switch_max = Ps_max - Ps_cond
+fs_max = Ps_switch_max/(Woff+Won)
